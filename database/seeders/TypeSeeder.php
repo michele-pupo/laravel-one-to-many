ec6609cf0724ee89b20d9bf55afdeb15a33fd5c5
michele-fabio-pupo <mikypupo@hotmail.it>
2024-04-23 14:49:25 +0200

    populated type table via seeders

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,18 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $types = [
+            'front-end',
+            'back-end'
+        ];
+
+        foreach ($types as $type) {
+
+            $newType = new Type();
+
+            $newType->title = $type;
+
+            $newType->save();
+        }
     }
 }
